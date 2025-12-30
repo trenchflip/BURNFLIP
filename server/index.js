859520181fa9ebea9c88d61e5465d9210a6a4b5e
BurnFlip <trenchparty@gmail.com>
2025-12-30 13:29:31 -0500

    Bind API server to 0.0.0.0 for Render

--- server/index.js
+++ server/index.js
@@ -414,7 +414,7 @@ app.post("/settle", rateLimit, async (req, res) => {
 });
 
 const PORT = Number(process.env.PORT) || 8787;
-const HOST = process.env.HOST || "127.0.0.1";
+const HOST = process.env.HOST || "0.0.0.0";
 
 app.listen(PORT, HOST, () => {
   console.log(`Server running on http://${HOST}:${PORT}`);
